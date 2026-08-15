c7434d1e576189c30164df530f5f4bb5b7a5a057
Trevor Simpson <Tsimpson2994@gmail.com>
2026-08-15 15:42:37 -0500

    initial commit

---
+++ src/main/java/com/stampede/flattire/FlattireApplication.java
@@ -0,0 +1,13 @@
+package com.stampede.flattire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlattireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlattireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stampede/flattire/FlattireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stampede.flattire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlattireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
